574924c8d53b317704f2fa4add32013dc2602746
Dreycen Foiles <foilesdreycen@gmail.com>
2020-09-03 14:33:25 -0500

    Rewrote SSP to inherit from Simulation class

--- SolutionSpaceProjection.py
+++ SolutionSpaceProjection.py
@@ -1,27 +1,38 @@
 import scipy.sparse as sp
 from scipy.linalg import inv, orth
+from Simulation import Simulation
 
 
-class SolutionSpace:
+class SolutionSpace(Simulation):
+
+	tol = 1e-3
 
 	def __init__(self, fmin, fmax):
 
-		self.Generate(fmin,fmax)
-
-	def Generate(self,fmin,fmax):
-
-		tol = 1e-3
+		self.Generate(fmin, fmax)
+		self.vec_list = []
 
 		for wavelength in [fmin, fmax]:
-			Esc = GetEsc(wavelength)
+			Esc = self.GetEsc(wavelength)
 			b = Esc.vec.FV().NumPy()
 			self.vec_list.append(b)
+
+		fmid = (fmin + fmax) / 2
+		error = self.Projection((fmin+fmid)/2, GetError=True)
+		if error < tol:
+			return
+		else:
+			Generate(fmin, fmax)
+
+	def Generate(self, fmin, fmax):
+
+		fmid = (fmin + fmax) / 2
 
 		error1 = self.Projection((fmin+fmid)/2, GetError=True)
 		error2 = self.Projection((fmid+fmax)/2, GetError=True)
 
 		if (error1 < tol) and (error2 < tol):
-			return 
+			return
 
 		elif error1 >= error2:
 			self.Generate(fmin, fmid)
@@ -30,8 +41,8 @@ class SolutionSpace:
 
 	def Projection(self, wavelength, GetError=False):
 
-		Esc = GridFunction(fes)
-		a, f = GetEsc(wavelength, mat=True)
+		Esc = GridFunction(self.fes)
+		a, f = self.GetEsc(wavelength, mat=True)
 		X = orth(np.column_stack(self.vec_list))
 		Xh = np.conjugate(X.T)
 		rows, cols, vals = a.mat.COO()
@@ -47,6 +58,8 @@ class SolutionSpace:
 		else:
 			Esc.vec.FV().NumPy()[:] = X@v
 			return Esc
+
+	# def Spectrum(self, )
 
 	
 
